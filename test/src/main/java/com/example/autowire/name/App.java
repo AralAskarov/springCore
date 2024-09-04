e4cc4e20b99ebdd1188b9ab639f7648571da8247
AralAskarov <askarov.10.09@gmail.com>
2024-09-05 01:17:33 +0500

    autowire by name/type/constructor

---
+++ test/src/main/java/com/example/autowire/name/App.java
@@ -0,0 +1,14 @@
+package com.example.autowire.name;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+//    psvm - public static void main
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("autowireByName.xml");
+        Car carr = (Car) context.getBean("car");
+        carr.displayDetails();
+    }
+}
